c78cd65d44846e830762a6ee14142f19991ac2f6
Balu Rama Chandra <baluramachandra90@gmail.com>
2023-03-14 12:21:58 +0700

    create new data models dim_merchants_revenues and add bank_code to dim_accounts

---
+++ target/compiled/dbt_piperider/models/marts/dim_merchants_revenue.sql
@@ -0,0 +1,18 @@
+WITH merchants_revenue AS (
+select
+    t2.year,
+    t2.month_of_year as month,
+    merchant_name,
+    count(*) as total_transactions,
+    sum(t1.amount) as total_amount
+from analytics_credit_debit_transaction.analytics_marts.fct_transactions t1
+join analytics_credit_debit_transaction.analytics_marts.dim_dates t2
+    on t1.date_key = t2.date_key
+join analytics_credit_debit_transaction.analytics_marts.dim_merchants t3
+    on t1.merchant_key = t3.merchant_key
+where t3.merchant_name <> 'No Merchant'
+group by 1, 2, 3
+order by 1, 2, 3
+)
+
+SELECT * FROM merchants_revenue
