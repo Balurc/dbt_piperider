b40a56a52fd9bb6e8c3a7dba68df6997d63f3f61
Balu Rama Chandra <baluramachandra90@gmail.com>
2023-03-19 15:23:12 +0700

    add dim_merchants revenue and renaming columns

--- models/staging/stg_accounts.sql
+++ models/staging/stg_accounts.sql
@@ -1,8 +1,8 @@
 WITH customer_accounts AS (
   SELECT DISTINCT 
     account_reference AS account_id,
-    provider_group_name,
-    -- bank_code,
+    provider_group_name AS bank_name,
+    bank_code,
     account_type,
     account_created_date,
     account_last_refreshed
