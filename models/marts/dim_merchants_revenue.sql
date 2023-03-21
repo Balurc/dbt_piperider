c562869d11461b8f6f43aec19140f983111c1b0f
Balu Rama Chandra <baluramachandra90@gmail.com>
2023-03-21 07:45:19 +0700

    changed data source and rebuild all the models

--- models/marts/dim_merchants_revenue.sql
+++ models/marts/dim_merchants_revenue.sql
@@ -5,10 +5,10 @@ select
     merchant_name,
     count(*) as total_transactions,
     sum(t1.amount) as total_amount
-from analytics_credit_debit_transaction.analytics_dev.fct_transactions t1
-join analytics_credit_debit_transaction.analytics_dev.dim_dates t2
+from {{ ref('fct_transactions') }} t1
+join {{ ref('dim_dates') }} t2
     on t1.date_key = t2.date_key
-join analytics_credit_debit_transaction.analytics_dev.dim_merchants t3
+join {{ ref('dim_merchants') }} t3
     on t1.merchant_key = t3.merchant_key
 where t3.merchant_name <> 'No Merchant'
 group by 1, 2, 3
