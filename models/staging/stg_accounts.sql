b1e7ef24a51ff2fedc9e3ae1c07af8cca66aefec
Balu Rama Chandra <baluramachandra90@gmail.com>
2023-03-21 10:00:51 +0700

    change metrics calculation methods

--- models/staging/stg_accounts.sql
+++ models/staging/stg_accounts.sql
@@ -2,7 +2,7 @@ WITH customer_accounts AS (
   SELECT DISTINCT 
     account_reference AS account_id,
     provider_group_name,
-    -- bank_code,
+    bank_code,
     account_type,
     account_created_date,
     account_last_refreshed
